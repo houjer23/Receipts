8f5c3eb620844bdede789337ec09b9bc104816dd
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-21 15:14:49 -0400

    Update Kiosk.java
    12 minutes to type

--- Kiosk.java
+++ Kiosk.java
@@ -1,2 +1,16 @@
-
-//your main interface for the project, with the main method
+import java.util.Scanner;
+public class Kiosk {
+	public static void main(String[] args) {
+		String[] items = {"cups of milk", "briskets", "beef", "lamb", "stacks of bread"}
+		double[] prices = {5.99, 6.99, 8.99, 10.99, 3.99};
+		int[] buy = new int[5];
+		Scanner scan = new Scanner(System.in);
+		for (int i = 0; i < 5; i ++) {
+			System.out.println(items[i] + " costs " + prices[i] + " dollars");
+			System.out.print ("How much " + items[i] + " do you want to buy? ");
+			buy[i] = scan.nextInt();
+		}
+		Printer ticket = new Printer(items, prices, buy);
+		ticket.print();
+	}
+}
