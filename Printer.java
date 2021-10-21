3544b405d677b9a74dc7d3d81b2fd94788e7606a
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-21 18:01:56 -0400

    Update Printer.java
    First of all, after receiving this problem, I thought about using some value to store those five items. I think that three arrays would be the best choice. The first array stores the shop items' names, which should be strings—the second array stores the shop items' price, which should be double. I would set the values of these two arrays by myself. In the third array, I will store how much the person will buy according to the user input, which should be int. In these three arrays, each index corresponds to each item. For example, if index 0 stores information about briskets, then index 0 of the three arrays all stores information about briskets. When I ask for user input, I have to loop through the index. Other than changing names and prices while looping each index, the structure should be similar each time. The process is similar when printing the ticket. After printing the header, I have to loop through each index and change quantity, names, and prices. Here is my method for printing the spaces between name and price: the size of the name plus the size of the spaces should be the same each time, so I can get the size of the spaces by the total size minus the size of the name. To print out the total cost, I have to have a variable that stores the total cost. The total cost variable increments each time while looping through each item.

--- Printer.java
+++ Printer.java
@@ -1,36 +1,45 @@
-public class Printer {
+public class Printer { // Class for printing out the ticket
 	private String[] items;
 	private double[] prices;
 	private int[] buy;
-	public Printer(String[] items, double[] prices, int[] buys) {
+	public Printer(String[] items, double[] prices, int[] buy) { // Construction of the class
 		this.items = items;
 		this.prices = prices;
 		this.buy = buy;
 	}
-	public print() {
-		for (int i = 0; i < 40; i ++) {
-			System.out.print("*");
-		}
-		System.out.println("\nWelcome to Kroger");
-		System.out.println("Here is your receipt\n");
+	public void print() { // Method used for printing the ticket
+		System.out.println("\n****************************************");
+		System.out.println("|Welcome to Kroger                     |");
+		System.out.println("|Here is your receipt                  |");
+		System.out.println("|                                      |");
+		System.out.print("|Qty        Description/Name      Price|");
+		System.out.println();
+		double sum = 0;
 		for (int i = 0; i < 5; i ++) {
 			if (buy[i] == 0) {
 				continue;
 			}
-			System.out.println("|" + buy[i]);
+			System.out.print("|" + buy[i]);
 			for (int j = 0; j < 10; j ++) {
 				System.out.print(" ");
 			}
 			System.out.print(items[i]);
-			for (int j = 0; j < 21 - items[i].length(); i ++) {
+			for (int j = 0; j < 21 - items[i].length(); j ++) {
 				System.out.print(" ");
 			}
-			double priceSum = buy * prices[i];
+			double priceSum = buy[i] * prices[i];
+			sum += priceSum;
 			if (priceSum < 10) {
-				System.out.println("$ " + printSum + "|");
+				System.out.println("$ " + priceSum + "|");
 			} else {
 				System.out.println("$" + priceSum + "|");
 			}
 		}
+		System.out.println("|                                      |");
+		System.out.println("|               Subtotal   $" + sum + "      |");
+		double tax = (double) Math.round(sum * 0.07 * 100) / 100;
+		System.out.println("|               Tax 7%     $ " + tax + "      |");
+		System.out.println("|               Total      $" + (tax + sum) + "      |");
+		System.out.println("****************************************");
 	}
 }
