22fe6f9dd3b66d47070ecd6b181010c08e5a439c
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-21 15:15:21 -0400

    Update Printer.java
    12 minutes to type

--- Printer.java
+++ Printer.java
@@ -1,21 +1,36 @@
-//Printer.java
-
-/* This is a java file 'stub' or piece of a file designed to get you started on 
-* learning to code in java, specifically with multiple classes and methods that rely on the other.
-*
-*For this assignment, I would like you to fork, modify and commit this project, while
-* adding the following elements to your code.
-*
-* The receipt printer file could take arguments or parametrs from another class
-* This file will be purely for output
-****************************************
-* 40 characters is a standard width    * 
-* for typical receipt paper, your task *
-* is to make output from the screen    *
-* look like it is printing to a small  *
-* receipt type printer, with all the   *
-* text formatted to 40 characters wide *
-*                                      *
-* Have fun with the assignment!!       *
-****************************************
-*/
+public class Printer {
+	private String[] items;
+	private double[] prices;
+	private int[] buy;
+	public Printer(String[] items, double[] prices, int[] buys) {
+		this.items = items;
+		this.prices = prices;
+		this.buy = buy;
+	}
+	public print() {
+		for (int i = 0; i < 40; i ++) {
+			System.out.print("*");
+		}
+		System.out.println("\nWelcome to Kroger");
+		System.out.println("Here is your receipt\n");
+		for (int i = 0; i < 5; i ++) {
+			if (buy[i] == 0) {
+				continue;
+			}
+			System.out.println("|" + buy[i]);
+			for (int j = 0; j < 10; j ++) {
+				System.out.print(" ");
+			}
+			System.out.print(items[i]);
+			for (int j = 0; j < 21 - items[i].length(); i ++) {
+				System.out.print(" ");
+			}
+			double priceSum = buy * prices[i];
+			if (priceSum < 10) {
+				System.out.println("$ " + printSum + "|");
+			} else {
+				System.out.println("$" + priceSum + "|");
+			}
+		}
+	}
+}
