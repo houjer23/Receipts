131bb32b4e689416a2abe1445405b98450b4deeb
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-22 09:17:11 -0400

    Update Printer.java
    Comments + better UI

--- Printer.java
+++ Printer.java
@@ -1,8 +1,12 @@
+/*
+ * Jerry Hou
+ * Class for printing out the ticket
+ */
 public class Printer { // Class for printing out the ticket
 	private Order userOrder;
 	public Printer(Order userOrder) { // Construction of the class
 		this.userOrder = userOrder;
-	}
+	} // end of Printer constructor
 	public void print() { // Method used for printing the ticket
 		System.out.println("\n****************************************");
 		System.out.println("|Welcome to Kroger                     |");
@@ -11,16 +15,12 @@ public class Printer { // Class for printing out the ticket
 		System.out.print("|Qty        Description/Name      Price|");
 		System.out.println();
 		double sum = 0;
-		for (int i = 0; i < 5; i ++) {
+		for (int i = 0; i < 5; i ++) { // Loop throught each index
 			if (userOrder.buy[i] == 0) {
 				continue;
 			}
-			System.out.print("|" + userOrder.buy[i]);
-			for (int j = 0; j < 10; j ++) {
-				System.out.print(" ");
-			}
-			System.out.print(userOrder.items[i]);
-			for (int j = 0; j < 21 - userOrder.items[i].length(); j ++) {
+			System.out.print("|" + userOrder.buy[i] + "          " + " " + userOrder.items[i]);
+			for (int j = 0; j < 21 - userOrder.items[i].length(); j ++) { // printing spaces between name and price
 				System.out.print(" ");
 			}
 			double priceSum = userOrder.buy[i] * userOrder.prices[i];
@@ -30,12 +30,12 @@ public class Printer { // Class for printing out the ticket
 			} else {
 				System.out.println("$" + priceSum + "|");
 			}
-		}
+		} // end of the for loop
 		System.out.println("|                                      |");
-		System.out.println("|               Subtotal   $" + sum + "      |");
-		double tax = (double) Math.round(sum * 0.07 * 100) / 100;
-		System.out.println("|               Tax 7%     $ " + tax + "      |");
-		System.out.println("|               Total      $" + (tax + sum) + "      |");
+		System.out.println("|               Subtotal   $" + String.format("%.2f", sum) + "      |");
+		double tax = (double) Math.round(sum * 0.07 * 100) / 100; // round tax to two decimals
+		System.out.println("|               Tax 7%     $ " + String.format("%.2f", tax) + "      |");
+		System.out.println("|               Total      $" + String.format("%.2f", tax + sum) + "      |");
 		System.out.println("****************************************");
-	}
+	} // end of printer method
 }
