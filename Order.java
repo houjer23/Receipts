674d69ea6057b07dbe008e0559ce7c2a1a14bb53
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-22 09:16:42 -0400

    Update Order.java
    Comments + better UI

--- Order.java
+++ Order.java
@@ -1,3 +1,8 @@
+/*
+ * Jerry Hou
+ * Order class stores three variables, each with different type
+ * It is used to easily pass as an argument
+ */
 import java.util.Scanner;
 public class Order { // Order class stores three variables, each with different type
 	String[] items = {"cups of milk", "briskets", "beef", "lamb", "stacks of bread"};
@@ -6,10 +11,10 @@ public class Order { // Order class stores three variables, each with different 
 	public Order() {} // constructor
 	public void buy() { // buy method asks for using input for quantity
 		Scanner scan = new Scanner(System.in);
-		for (int i = 0; i < 5; i ++) {
+		for (int i = 0; i < 5; i ++) { // loop through each index, ask for quantity for each item
 			System.out.println(items[i] + " costs " + prices[i] + " dollars");
 			System.out.print ("How much " + items[i] + " do you want to buy? ");
 			buy[i] = scan.nextInt();
-		}
-	}
-}
+		} // end of for loop
+	} // end of buy method
+} // end of order class
