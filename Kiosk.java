59b137280f2fa19b8711ec42434dd94eb069a9ca
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-22 09:00:35 -0400

    Update Kiosk.java
    Order class stores three variables, each with different type

--- Kiosk.java
+++ Kiosk.java
@@ -1,16 +1,9 @@
 import java.util.Scanner;
 public class Kiosk { // The main class, gets input and print out ticket
 	public static void main(String[] args) { // Where the code runs
-		String[] items = {"cups of milk", "briskets", "beef", "lamb", "stacks of bread"};
-		double[] prices = {5.99, 6.99, 8.99, 10.99, 3.99};
-		int[] buy = new int[5];
-		Scanner scan = new Scanner(System.in);
-		for (int i = 0; i < 5; i ++) {
-			System.out.println(items[i] + " costs " + prices[i] + " dollars");
-			System.out.print ("How much " + items[i] + " do you want to buy? ");
-			buy[i] = scan.nextInt();
-		}
-		Printer ticket = new Printer(items, prices, buy);
+		Order userOrder = new Order();
+		userOrder.buy();
+		Printer ticket = new Printer(userOrder);
 		ticket.print();
 	}
 }
