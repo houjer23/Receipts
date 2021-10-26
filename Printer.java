e288f83744f8e85a8c89c6ffac8d1e2e3c3cb9ec
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-26 08:30:31 -0400

    Update Printer.java
    Better UI

--- Printer.java
+++ Printer.java
@@ -8,11 +8,11 @@ public class Printer { // Class for printing out the ticket
 		this.userOrder = userOrder;
 	} // end of Printer constructor
 	public void print() { // Method used for printing the ticket
-		System.out.println("\n****************************************");
-		System.out.println("|Welcome to Kroger                     |");
-		System.out.println("|Here is your receipt                  |");
-		System.out.println("|                                      |");
-		System.out.print("|Qty        Description/Name      Price|");
+		System.out.println("\n*****************************************");
+		System.out.println("|Welcome to Kroger                      |");
+		System.out.println("|Here is your receipt                   |");
+		System.out.println("|                                       |");
+		System.out.print("|Qty        Description/Name      Price |");
 		System.out.println();
 		double sum = 0;
 		for (int i = 0; i < 5; i ++) { // Loop throught each index
@@ -31,11 +31,11 @@ public class Printer { // Class for printing out the ticket
 				System.out.println("$" + priceSum + "|");
 			}
 		} // end of the for loop
-		System.out.println("|                                      |");
-		System.out.println("|               Subtotal   $" + String.format("%.2f", sum) + "      |");
+		System.out.println("|                                       |");
+		System.out.println("|               Subtotal   $" + String.format("%.2f", sum) + "       |");
 		double tax = (double) Math.round(sum * 0.07 * 100) / 100; // round tax to two decimals
-		System.out.println("|               Tax 7%     $ " + String.format("%.2f", tax) + "      |");
-		System.out.println("|               Total      $" + String.format("%.2f", tax + sum) + "      |");
-		System.out.println("****************************************");
+		System.out.println("|               Tax 7%     $ " + String.format("%.2f", tax) + "       |");
+		System.out.println("|               Total      $" + String.format("%.2f", tax + sum) + "       |");
+		System.out.println("*****************************************");
 	} // end of printer method
 }
